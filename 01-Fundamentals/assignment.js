b8c30beeeb05e3fcdfca4776697661069453af2f
Abhishek Madiwale <abhishek.madiwale@gmail.com>
2023-02-20 23:43:47 +0530

    Different types of data types of JS inside dataTypes file

---
+++ 01-Fundamentals/assignment.js
@@ -0,0 +1,7 @@
+let country = "India";
+let continent ="Asia";
+let population = "140 Billion";
+let isIsland = false // Boolean value assign to a variable
+const language= "Hindi";
+console.log("Country is : ",country,", Continent is: ", continent,", Population is: ", population, "is it island: ", isIsland)
+console.log("Language is: ", language)
---
+++ 01-Fundamentals/dataTypes.js
@@ -0,0 +1,28 @@
+// Some of the important Data types
+// 1) Number = Floating point numbers used for decimal and integers
+let age = 26;
+console.log("Number example: ", age);
+
+// 2) String = Sequence of characters used for text
+let firstName = "Abhishek";
+console.log("String example: ", firstName);
+
+// 3) Boolean = Logical type that can only be true or false used for taking the decisions
+let fullAge = true;
+console.log("Boolean example: ", fullAge);
+
+// 4) Undefined = Values taken by a variable that is not yet defined ("empty value")
+let children;
+console.log("Undefined example: ", children);
+
+// 5) Null = Similar like undefined, but we need to mentioned that variable is null
+// in undefined we don't need to defined that it is undefined
+let address = null;
+console.log("Null example: ", address);
+
+// Dynamic variable example
+firstName = "Shreyas";
+console.log("Dynamic change variable example: ", firstName);
+address="India";
+console.log("Dynamic address: ", address);
+console.log(typeof address);
